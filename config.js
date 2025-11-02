159e87b1c3f1e20a0a5cb4a6371cf96c53124220
DominicDev <s0l0estudi4r@gmail.com>
2025-11-02 14:26:31 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'KAISER CHIEFS';
+export let nameSongs = [
+	"RUBY", /*1*/
+	"MY LIFE", /*2*/
+	"CHELSEA DAGGER", /*3*/
+	"OWN WAY", /*4*/
+	"OH MY GOD", /*5*/
+	"COMING HOME", /*6*/
+	"I PREDICT A RIOT ", /*7*/
+	"FALLING AWAKE", /*8*/
+	"LISTEN TO YOUR HEAD", /*9*/
+	"NEVER MISS A BEAT", /*10*/
+	"FEELING ALRIGHT", /*11*/
+	"BEAUTIFUL GIRL", /*12*/
+	"HOW 2 DANCE", /*13*/
+	"BURNING IN FLAMES", /*14*/
+	"THE LADS", /*15*/
+	"THE JOB CENTRE SHUFFLE", /*16*/
+	"REASONS TO STAY ALIVE", /*17*/
+	"SENTIMENTAL LOVE SONGS", /*18*/
+	"JEALOUSY", /*19*/
+	"EVERYDAY I LOVE YOU ", /*20*/
+]
